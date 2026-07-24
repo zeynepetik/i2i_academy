6f2444f711e1fc39a6b2a9972841ee0e1aa4c20c
zeynepetik <etikzeynep22@gmail.com>
2026-07-24 11:35:50 +0300

    added vercel domain to corsconfig

--- cryptopal/CryptoPal Core/Backend/cryptopal/src/main/java/com/i2iacademy/cryptopal/common/CorsConfig.java
+++ cryptopal/CryptoPal Core/Backend/cryptopal/src/main/java/com/i2iacademy/cryptopal/common/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000",
+                            "https://i2i-academy.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
